a4cf84b3410fb33362aad247a1cddcc307218da2
Maria Sofia Caucota <masocaucota@gmail.com>
2022-11-23 01:45:22 -0300

    first commit

---
+++ src/components/itemListContainer/ItemListContainer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { PropTypes } from "prop-types";
+
+const ItemListContainer = (prop) => {
+    return(
+        <div className="prop">Hola, {prop.name}, Bienvenido.</div>
+    );
+};
+
+
+
+export default ItemListContainer;
